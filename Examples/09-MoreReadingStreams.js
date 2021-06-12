7c490c5554df8a0274c27650e4b8fa73ce7f9960
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-06-12 18:55:15 +0200

    Read a bigger text file using streams

--- Examples/09-MoreReadingStreams.js
+++ Examples/09-MoreReadingStreams.js
@@ -1,1 +1,27 @@
 // using streams to read files
+let http = require("http");
+let fs = require("fs");
+
+// READ THE WHOLE FILE AT ONCE - NOT RECOMMENDED
+/* http
+  .createServer(function (request, response) {
+    const text = fs.readFileSync("./BiggerTextFile.txt", "utf8");
+    response.end(text);
+  })
+  .listen(5000); */
+
+// READ THE FILE IN CHUNCKS, USING STREAMS
+http
+  .createServer(function (request, response) {
+    const fileStream = fs.createReadStream("./BiggerTextFile.txt", {
+      highWaterMark: 90000,
+      encoding: "utf8",
+    });
+    fileStream.on("open", () => {
+      fileStream.pipe(response);
+    });
+    fileStream.on("error", (error) => {
+      response.end(error);
+    });
+  })
+  .listen(5000);
