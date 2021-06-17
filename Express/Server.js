0e345d55980512af2c8c9448d0cda5b8eda66e5c
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-06-17 19:04:07 +0200

    Passed the content of an thml document to the server's response, instead of a string

--- Express/Server.js
+++ Express/Server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const { readFileSync } = require("fs");
+
 // the callback method gets invoked every time the user goes to our server
 // it has 2 parameters, request and response (we can call them however we want)
 // first paramater refrences the request object
@@ -25,7 +27,7 @@ server.listen(3000); */
 
 // -------------------------------------------  ADDING MORE DETAILS TO THE REQUEST WE WE SEND BACK TO THE BROWSER
 
-const server = http.createServer((request, response) => {
+/* const server = http.createServer((request, response) => {
   // console.log(request.method);
   //console.log(request.url);
 
@@ -43,6 +45,36 @@ const server = http.createServer((request, response) => {
     response.end();
   } else {
     // if the urls is anything else, we return an error message
+    response.writeHead(404, { "content-type": "text/html" });
+    response.write(
+      "<h1>We are sorry, the page you are looking for doesn't exist!</h1>"
+    );
+    response.end();
+  }
+});
+
+server.listen(3000); */
+
+// -------------------------------------------  PASSING THE CONTENT OF A HTML DOCUMENT TO THE RESPONSE
+
+// get the content of TestIndex.html
+
+const homePage = readFileSync("./Express/TestIndex.html");
+const server = http.createServer((request, response) => {
+  // console.log(request.method);
+  //console.log(request.url);
+
+  const requestedUrl = request.url; // we get the url the user requests
+
+  if (requestedUrl === "/") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.write(homePage);
+    response.end();
+  } else if (requestedUrl === "/about") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.write("<h1>Hello, this is the ABOUT page!</h1>");
+    response.end();
+  } else {
     response.writeHead(404, { "content-type": "text/html" });
     response.write(
       "<h1>We are sorry, the page you are looking for doesn't exist!</h1>"
