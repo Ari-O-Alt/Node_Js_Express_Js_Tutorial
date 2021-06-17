46f25ba6739d66f53c804f226dfd9f26b84ccc1d
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-06-17 20:03:19 +0200

    Served HTML + CSS + JS + Logo to the browser

--- Express/Server.js
+++ Express/Server.js
@@ -59,23 +59,64 @@ server.listen(3000); */
 
 // get the content of TestIndex.html
 
-const homePage = readFileSync("./Express/TestIndex.html");
+/* const homePage = readFileSync("./Express/TestIndex.html");
 const server = http.createServer((request, response) => {
-  // console.log(request.method);
-  //console.log(request.url);
-
-  const requestedUrl = request.url; // we get the url the user requests
+  const requestedUrl = request.url;
 
   if (requestedUrl === "/") {
     response.writeHead(200, { "content-type": "text/html" });
     response.write(homePage);
     response.end();
-  } else if (requestedUrl === "/about") {
-    response.writeHead(200, { "content-type": "text/html" });
-    response.write("<h1>Hello, this is the ABOUT page!</h1>");
-    response.end();
   } else {
     response.writeHead(404, { "content-type": "text/html" });
+    response.write(
+      "<h1>We are sorry, the page you are looking for doesn't exist!</h1>"
+    );
+    response.end();
+  }
+});
+
+server.listen(3000); */
+
+// -------------------------------------------  PASSING THE CONTENT OF ALL FILES IN NAVBAR-APP TO  THE RESPONSE
+
+const homePage = readFileSync("./Express/navbar-app/index.html"); //we get the content of index.html
+const homeStyles = readFileSync("./Express/navbar-app/styles.css"); //we get the content of style.css
+const homeLogic = readFileSync("./Express/navbar-app/browser-app.js"); //we get the content of browser-app.js
+const homeLogo = readFileSync("./Express/navbar-app/logo.svg"); //we get the content of logo
+
+const server = http.createServer((request, response) => {
+  const requestedUrl = request.url;
+  console.log(requestedUrl);
+
+  // ------ HOME page -------
+  if (requestedUrl === "/") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.write(homePage);
+    response.end();
+    // ------ ABOUT page -------
+  } else if (requestedUrl === "/about") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.write("<h1>This is the ABOUT page!</h1>");
+    response.end();
+    // ------ HOME page/ CSS -------
+  } else if (requestedUrl === "/styles.css") {
+    response.writeHead(200, { "content-type": "text/css" }); // don't forget to change the content type
+    response.write(homeStyles);
+    response.end();
+    // ------ HOME page/ JS -------
+  } else if (requestedUrl === "/browser-app.js") {
+    response.writeHead(200, { "content-type": "text/javascript" }); // don't forget to change the content type
+    response.write(homeLogic);
+    response.end();
+  }
+  // ------ HOME page/ LOGO -------
+  else if (requestedUrl === "/logo.svg") {
+    response.writeHead(200, { "content-type": "image/svg+xml" }); // don't forget to change the content type
+    response.write(homeLogo);
+    response.end();
+  } else {
+    response.writeHead(404, { "content-type": "text/html" }); // don't forget to change the content type
     response.write(
       "<h1>We are sorry, the page you are looking for doesn't exist!</h1>"
     );
