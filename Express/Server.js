820d6ee2dfde2ca714317c35d0be7c034ced4e17
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-06-18 23:28:45 +0200

    Served the HOME page of our website using Express

--- Express/Server.js
+++ Express/Server.js
@@ -2,6 +2,17 @@ const express = require("express"); // import the module
 
 const app = express(); // invoke it
 
+app.get("/", (request, response) => {
+  // we serve a string is he user tries to access the home page
+  console.log("User hit the HOME page!");
+  response.send("This is the home page!");
+});
+
+app.listen(3000, () => {
+  // we 're listening to port 3000
+  console.log("The server is listening on post 3000...");
+});
+
 // app.get
 // app.post
 // app.put
