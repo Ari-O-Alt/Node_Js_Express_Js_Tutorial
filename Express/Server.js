3f3c5b57c42484ed6d99101a65c72a2d90f55d63
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-11 11:35:15 +0200

    Used query params

--- Express/Server.js
+++ Express/Server.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const express = require("express");
 const app = express();
 const { products } = require("./Data.js");
@@ -25,9 +25,10 @@ app.get("/api/products", (request, response) => {
   response.json(singleProducts);
 }); */
 
+// the colon tells us a route parameter is used
 app.get("/api/products/:productId", (request, response) => {
-  console.log(request);
-  console.log(request.params);
+  // console.log("request", request);
+  // console.log("params", request.params);
   const { productId } = request.params; // we deconstruct the productId on the params
   const singleProduct = products.find((product) => {
     // if there's a product with the deconstructed product id, we send it back to the user
@@ -46,11 +47,17 @@ app.get("/api/products/:productId", (request, response) => {
 
 // Trying to access an URL with two dynamic params
 // No matter the value of the dynamic params, we send the same reponse back
+// Dummy example, it doesn't return anything since we don't have a setup for these routes
 app.get("/api/products/:productId/reviews/:reviewId", (request, response) => {
-  console.log(request.params);
+  // console.log(request.params);
   response.send("Just a placeholder!");
 });
 
-app.app.listen(3000, () => {
+app.get("/api/v1/query", (request, response) => {
+  console.log(request.query);
+  response.send("Hello world!");
+});
+
+app.listen(3000, () => {
   console.log("The server is listening to port 3000...");
 });
